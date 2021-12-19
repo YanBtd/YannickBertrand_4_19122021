9b417aa59cea729a537573e3d1cf59ecced34c27
YanBtd <ybertrand@free.fr>
2021-12-19 10:16:34 +0100

    Ajout fonctionnalité Fermer la modale

--- modal.js
+++ modal.js
@@ -1,23 +1,37 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
 
-// DOM Elements
+/* DOM Elements **************************/
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll("#close");
+
+
+/* EventListeners ********************/
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeModalBtn.forEach(elmt => elmt.addEventListener("click", closeModal));
+
+
+/* Functions ******************************/
+
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+    modalbg.style.display = "none";
+}
 
