f5db67785dace254651212f797cd96b0bfdb826d
YanBtd <ybertrand@free.fr>
2021-12-19 14:37:46 +0100

    Ajout attributs for + refact code html

--- modal.js
+++ modal.js
@@ -1,3 +1,24 @@
+/***** CONSTANTES *****/ 
+
+/* DOM Elements */
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const closeModalBtn = document.querySelectorAll("#close-modal");
+
+
+/***** EVENTLISTENERS *****/ 
+
+// launch modal event click
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event click
+closeModalBtn.forEach(elmt => elmt.addEventListener("click", closeModal));
+
+
+/***** FUNCTIONS *****/ 
+
+// nav responsive button
 function editNav() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -6,24 +27,6 @@ function editNav() {
         x.className = "topnav";
     }
 }
-
-/* DOM Elements **************************/
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.querySelectorAll("#close");
-
-
-/* EventListeners ********************/
-
-// launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-
-// close modal event
-closeModalBtn.forEach(elmt => elmt.addEventListener("click", closeModal));
-
-
-/* Functions ******************************/
 
 // launch modal form
 function launchModal() {
