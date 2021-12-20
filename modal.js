8d8174351e05f68b8fdc2e90491f4cf3fc0c1d51
YanBtd <ybertrand@free.fr>
2021-12-20 09:51:25 +0100

    Implémentation entrée prenom du formulaire

--- modal.js
+++ modal.js
@@ -1,24 +1,39 @@
-/***** CONSTANTES *****/ 
+/***** CONSTANTES *****/
 
 /* DOM Elements */
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeModalBtn = document.querySelectorAll("#close-modal");
+const MODALBG = document.querySelector(".bground");
+const MODALBTN = document.querySelectorAll(".modal-btn");
+const FORMDATA = document.querySelectorAll(".formData");
+const CLOSEMODALBTN = document.querySelectorAll("#close-modal");
+const SUCCESSMESSAGEDIV = document.querySelectorAll("#success-message");
+const FORM = document.querySelectorAll("#form");
+const SUCCESSCLOSEBTN = document.querySelectorAll("#success-close-btn");
+
+/* FORM Elements */
+const PRENOM = document.getElementById("first");
+const NOM = document.getElementById("last");
+const EMAIL = document.getElementById("email");
+const NAISSANCE = document.getElementById("birthdate");
+const QUANTITE = document.getElementById("quantity");
+const VILLE = document.querySelector("input[type=radio]");
+const CONDITIONS = document.getElementById("checkbox1");
 
 
-/***** EVENTLISTENERS *****/ 
+/***** EVENTLISTENERS *****/
 
-// launch modal event click
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+// ouverture formulaire modal au click Je M'inscris
+MODALBTN.forEach((btn) => btn.addEventListener("click", launchModal));
 
-// close modal event click
-closeModalBtn.forEach(elmt => elmt.addEventListener("click", closeModal));
+// fermeture formulaire modal au click sur la croix
+CLOSEMODALBTN.forEach(elmt => elmt.addEventListener("click", closeModal));
+
+// appel fonction de validation du form au click sur C'est parti
+FORM.forEach(elmt => elmt.addEventListener("submit", validate));
 
 
-/***** FUNCTIONS *****/ 
+/***** FUNCTIONS *****/
 
-// nav responsive button
+// affichage bouton de nav 
 function editNav() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -28,13 +43,33 @@ function editNav() {
     }
 }
 
-// launch modal form
+// fonction d'affichage du form modal
 function launchModal() {
-    modalbg.style.display = "block";
+    MODALBG.style.display = "block";
 }
 
-// close modal form
+// fonction de fermeture du form modal
 function closeModal() {
-    modalbg.style.display = "none";
+    MODALBG.style.display = "none";
 }
 
+// VALIDATIONS
+
+// validation prenom
+function isTailleOk(currentLength, minLength) {
+    return currentLength >= minLength;
+}
+function isPrenomValid() {
+    let isValid = isTailleOk(PRENOM.value.length, 2);
+    return isValid;
+}
+function validate(e) {
+    e.preventDefault();
+    let prenom = isPrenomValid();
+    if (prenom) {
+        alert("prenom valide");
+    } else {
+        alert("prenom invalide");
+    }
+    console.log(prenom);
+}
