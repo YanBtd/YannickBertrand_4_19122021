0d6b24a118041b7c8586dfba4c35c23bbdac9e3e
YanBtd <ybertrand@free.fr>
2021-12-28 12:50:59 +0100

    Création des répertoires css, js et img et modif index.html en conséquence

--- js/modal.js
+++ js/modal.js
@@ -22,7 +22,7 @@ const CONDITIONS = document.getElementById("checkbox1");
 const NOMBREREGEX = /^\+?(0|[1-9]\d*)$/;
 const EMAILREGEX = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
 const NAISSANCEREGEX = /^(19|20)\d{2}[-](0?[1-9]|1[012])[-](0[1-9]|[12]\d|3[01])$/;
-// const TEXTEREGEX = /^[A-Za-z]+$/;
+// const TEXTEREGEX = /^[A-Za-z]+$/; // pas de prise en compte des accents
 const TEXTEREGEX = /^[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ.\s-]{2,25}$/;
 
 
@@ -59,6 +59,7 @@ function editNav() {
         x.className = "topnav";
     }
 }
+
 // fonction d'affichage 
 function launchModal() {
     // affichage du background
@@ -66,6 +67,7 @@ function launchModal() {
     // affichage du form modal
     FORM[0].style.display = "block";
 }
+
 // fonction de fermeture 
 function closeModal() {
     // fermeture du form modal (via le background)
@@ -73,6 +75,7 @@ function closeModal() {
     // fermeture fenêtre modale de confirmation de réservation
     SUCCESSMESSAGEDIV[0].style.display = "none";
 }
+
 // affichage fenêtre de confirmation 
 function afficherComfirmation() {
     // calcul hauteur du form
@@ -92,44 +95,56 @@ function isPrenomValid() {
     let isValid = isValeurValideRegex(PRENOM.value, TEXTEREGEX);
     let inputPrenom = new Input(PRENOM, "Veuillez entrer 2 caractères ou plus pour le champ du Prénom.");
     enleverOuAfficherErreur(inputPrenom, isValid);
+
     return isValid;
 }
+
 // validation nom
 function isNomValid() {
     // let isValid = isTailleOk(NOM.value.length, 2);
     let isValid = isValeurValideRegex(NOM.value, TEXTEREGEX);
     let inputNom = new Input(NOM, "Veuillez entrer 2 caractères ou plus pour le champ du Nom.");
     enleverOuAfficherErreur(inputNom, isValid);
+
     return isValid;
 }
+
 // validation nombre de tournois
 function isNombreValid() {
     let isValid = isValeurValideRegex(NOMBRE.value, NOMBREREGEX);
     let inputNombre = new Input(NOMBRE, "Veuillez entrer un Nombre.");
     enleverOuAfficherErreur(inputNombre, isValid);
+
     return isValid;
 }
+
 // validation email
 function isEmailValid() {
     let isValid = isValeurValideRegex(EMAIL.value, EMAILREGEX);
     let inputEmail = new Input(EMAIL, "Veuillez entrer une adresse E-mail valide.");
     enleverOuAfficherErreur(inputEmail, isValid);
+
     return isValid;
 }
+
 // validation date de naissance
 function isNaissanceValid() {
     let isValid = isValeurValideRegex(NAISSANCE.value, NAISSANCEREGEX);
     let inputNaissance = new Input(NAISSANCE, "Veuillez entrer votre Date de naissance.");
     enleverOuAfficherErreur(inputNaissance, isValid);
+
     return isValid;
 }
+
 // validation ville du tournoi
 function isVilleValid() {
     let isValid = isRadioChecked();
     let inputVille = new Input(VILLE, "Veuillez choisir la Ville du tournoi.");
     enleverOuAfficherErreur(inputVille, isValid);
+
     return isValid;
 }
+
 // validation conditions d'utilisation
 function isConditionsValid() {
     let isValid = isCheckboxChecked("checkbox1");
@@ -151,7 +166,7 @@ function validerForm(e) {
     let ville = isVilleValid();
     let conditions = isConditionsValid();
     // exécution des fonctions de validation
-    let isFormValid = prenom && nom && nombre && email && naissance && ville && conditions;
+    let isFormValid = prenom && nom && nombre && email && naissance && ville && conditions; 
     if (isFormValid) {
         // appel fonction d'affichage
         afficherComfirmation();
@@ -162,10 +177,6 @@ function validerForm(e) {
 
 // OUTILS
 
-// // test valeur saisie utilisateur 
-// function isTailleOk(currentLength, minLength) {
-//     return currentLength >= minLength;
-// }
 // appel de la fonction adéquate si erreur ou non
 function enleverOuAfficherErreur(elmt, isValid) {
     if (isValid) {
@@ -174,14 +185,17 @@ function enleverOuAfficherErreur(elmt, isValid) {
         elmt.afficherErreur();
     }
 }
+
 // comparaison valeur saisie et regex
 function isValeurValideRegex(valeur, regex) {
     return regex.test(valeur);
 }
+
 // test si bouton-radio ville coché
 function isRadioChecked() {
     return document.querySelectorAll("input[type=radio]:checked").length > 0;
 }
+
 // test si case conditions d'utilisation cochée
 function isCheckboxChecked(id) {
     return document.getElementById(id).checked;
